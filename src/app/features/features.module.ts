63b64a7a2e90715a8aaea3387825a76b81545d59
Yurchenko Pavlo <yurchenkopavlo3@gmail.com>
2024-01-06 20:54:13 +0200

    finished ui part of main page

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -3,17 +3,20 @@ import { CommonModule } from '@angular/common';
 import {NavbarModule} from "./navbar/navbar.module";
 import {HeroModule} from "./hero/hero.module";
 import {TrendingModule} from "./trending/trending.module";
+import { FooterComponent } from './footer/components/footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FooterComponent
+  ],
   imports: [
     CommonModule,
     NavbarModule,
     HeroModule,
     TrendingModule
   ],
-  exports:[NavbarModule,HeroModule,TrendingModule]
+    exports: [NavbarModule, HeroModule, TrendingModule, FooterComponent]
 })
 export class FeaturesModule { }
